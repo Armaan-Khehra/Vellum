a6c91394ae6425b7c660f96d2a6763940cb67ca6
Armaan Khehra <armaan.txt@gmail.com>
2024-10-10 16:05:43 +0530

    UnaryOpNode is now placed outside BinOpNode. The factor method now calls self.expr() for parsing parentheses correctly. Error handling in run ensures that errors in the parsing stage are captured and returned for debugging.

--- Syntho/basic.py
+++ Syntho/basic.py
@@ -35,12 +35,12 @@ class Position:
         self.fn = fn
         self.ftxt = ftxt
 
-    def advance(self, current_char):
+    def advance(self, current_char=None):
         self.idx += 1
         self.col += 1
 
         if current_char == '\n':
-            self.ln += 1  # Fixed typo
+            self.ln += 1
             self.col = 0
 
         return self
@@ -63,7 +63,7 @@ class Token:
     def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
- 
+     
         if pos_start:
             self.pos_start = pos_start.copy()
             self.pos_end = pos_start.copy()
@@ -131,7 +131,7 @@ class Lexer:
 
         while self.current_char != None and self.current_char in DIGITS + '.':
             if self.current_char == '.':
-                if dot_count > 0: break  # Only allow one dot
+                if dot_count > 0: break
                 dot_count += 1
                 num_str += '.'
             else:
@@ -141,11 +141,9 @@ class Lexer:
         if dot_count == 0:
             return Token(TT_INT, int(num_str), pos_start, self.pos)
         else:
-            return Token(TT_FLOAT, float(num_str, pos_start, self.pos))
-        
+            return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
 
-#NODES
-
+# NODES
 class NumberNode:
     def __init__(self, tok) -> None:
         self.tok = tok
@@ -161,34 +159,40 @@ class BinOpNode:
     
     def __repr__(self) -> str:
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+    
+class UnaryOpNode:
+    def __init__(self, op_tok, node):
+        self.op_tok = op_tok
+        self.node = node
 
-#PARSE RESULT
+    def __repr__(self) -> str:
+        return f'({self.op_tok}, {self.node})'
 
+# PARSE RESULT
 class ParseResult:
     def __init__(self) -> None:
         self.error = None
         self.node = None
 
     def register(self, res):
-        if isinstance(res, ParseResult()):
+        if isinstance(res, ParseResult):
             if res.error: self.error = res.error
             return res.node
         return res
     
     def success(self, node):
-        self.none = node
+        self.node = node
         return self
 
     def failure(self, error):
         self.error = error
         return self
     
-#PARSER
-
+# PARSER
 class Parser:
     def __init__(self, tokens) -> None:
         self.tokens = tokens
-        self.tok_idx = 1
+        self.tok_idx = -1
         self.advance()
 
     def advance(self):
@@ -199,18 +203,43 @@ class Parser:
     
     def parse(self):
         res = self.expr()
+        if not res.error and self.current_tok.type != TT_EOF:
+            return res.failure(InvalidSyntaxError(
+                self.current_tok.pos_start, self.current_tok.pos_end,
+                "Expected '+', '-', '*' or '/'"
+            ))
+        
         return res
     
-    ################################
-    
+    #########################################
+
     def factor(self):
         res = ParseResult()
         tok = self.current_tok
 
-        if tok.type in (TT_INT, TT_FLOAT):
+        if tok.type in (TT_PLUS, TT_MINUS):
+            res.register(self.advance())
+            factor = res.register(self.factor())
+            if res.error: return res
+            return res.success(UnaryOpNode(tok, factor))
+
+        elif tok.type in (TT_INT, TT_FLOAT):
             res.register(self.advance())
             return res.success(NumberNode(tok))
         
+        elif tok.type == TT_LPAREN:
+            res.register(self.advance())
+            expr = res.register(self.expr())  # Fix: call self.expr()
+            if res.error: return res
+            if self.current_tok.type == TT_RPAREN:
+                res.register(self.advance())
+                return res.success(expr)
+            else:
+                return res.failure(InvalidSyntaxError(
+                    self.current_tok.pos_start, self.current_tok.pos_end,
+                    "Expected ')'"
+                ))
+
         return res.failure(InvalidSyntaxError(
             tok.pos_start, tok.pos_end,
             "Expected int or float"
@@ -222,7 +251,7 @@ class Parser:
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
     
-    ################################
+    #########################################
 
     def bin_op(self, func, ops):
         res = ParseResult()
@@ -233,20 +262,23 @@ class Parser:
             op_tok = self.current_tok
             res.register(self.advance())
             right = res.register(func())
-            if res.error : return res
-            left = BinOpNode(left, op_tok, right)   
+            if res.error: return res
+            left = BinOpNode(left, op_tok, right)
+        
         return res.success(left)
 
 # The run function
 def run(fn, text):
-    #Generate tokens
+    # Generate tokens
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
     if error: return None, error
 
-    #Generate AST
+    # Generate AST
     parser = Parser(tokens)
     ast = parser.parse()
 
+    if ast.error:  # Debugging in case of errors
+        return None, ast.error
 
-    return ast, None
+    return ast.node, None
