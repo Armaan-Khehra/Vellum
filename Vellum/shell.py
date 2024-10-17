f558b09ae489e7c2f72fe77363d45af65620cf18
Armaan Khehra <armaan.txt@gmail.com>
2024-10-17 22:14:24 +0530

    Added var

--- Vellum/shell.py
+++ Vellum/shell.py
@@ -1,15 +1,8 @@
 import basic
 
 while True:
-    text = input('basic > ')
-    
-    # If no input is provided, just continue the loop
-    if text.strip() == "":
-        continue
-    
-    result, error = basic.run('<stdin>', text)
+	text = input('basic > ')
+	result, error = basic.run('<stdin>', text)
 
-    if error: 
-        print(error.as_string())
-    else: 
-        print(result)
+	if error: print(error.as_string())
+	else: print(result)
